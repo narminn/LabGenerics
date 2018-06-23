764f8d7072b362ff13be6b3137f2148596fbad69
narminn <nasirovanarmin96@gmail.com>
2018-06-23 12:34:28 +0400

    lab tapshirigi

---
+++ LabGenerics/GenericList.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabGenerics
+{
+    class GenericList<T>:IEnumerable<T>
+    {
+        T[] array = new T[2];
+        int index = 0;
+
+        public void Add(T value)
+        {
+            if (index==array.Length)
+            {
+                Resize();
+            }
+            array[index] = value;
+            index++;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)array).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return array.GetEnumerator();
+        }
+
+        void Resize()
+        {
+            T[] tempArr = new T[array.Length + 1];
+            Array.Copy(array, tempArr, array.Length);
+            array =tempArr;
+        }
+
+        public bool Contains(T value)
+        {
+            bool x = false;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Equals(value))
+                {
+                    
+                     x=true;
+                    break;
+                }
+              
+               
+            }
+            return x;
+        }
+
+        public void Remove(T value)
+        {
+            if (array.Contains(value))
+            {
+                int y = Array.IndexOf(array, value);
+                T[] temp = new T[array.Length - 1];
+                for (int i = 0; i < array.Length-1; i++)
+                {
+                    if (array[i].Equals(array[y]))
+                    {
+                        
+                        continue;
+                        
+                    }
+                    temp[i] = array[i];
+                }
+                array = temp;
+            }
+            else
+            {
+                Console.WriteLine("bele bir data yoxdur");
+            }
+
+        }
+       
+    }
+}
---
+++ LabGenerics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<string> stringlist = new GenericList<string>();
+            stringlist.Add("Suleyman");
+            stringlist.Add("Naib");
+            stringlist.Add("Gunay");
+            stringlist.Add("Nermin");
+            stringlist.Add("FazilAbi");
+            //foreach (var item in stringlist)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Console.WriteLine(stringlist.Contains("heci"));
+            stringlist.Remove("Nermin");
+
+            foreach (var item in stringlist)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
